88f4ba21de8510f312fd6b72e7859f1848c99b1f
xyguo_mac <xyguo.ubd@gmail.com>
2019-03-22 23:57:11 -0400

    Unified interfaces. And interfaces for RMC model.

--- utils/grader.py
+++ utils/grader.py
@@ -30,33 +30,47 @@ class MRMGrader(Grader):
         return regression_mixture_grad(X, Y, P, P, self.sigma_)
 
 
-def gaussian_mixture_grad(X, mu, mu0, sigma):
-    """Compute the gradient on data set X w.r.t. the estimated mean mean
+class RMCGrader(Grader):
+    """Gradient computer for Regression with Missing Covariance model"""
+    def __init__(self, sigma=1):
+        self.sigma_ = sigma
+
+    def gradient(self, X, Y, P):
+        return regression_without_cov_grad(X, Y, P, P, self.sigma_)
+
+
+def gaussian_mixture_grad(X, beta, beta0, sigma):
+    """Compute the gradient on data set X w.r.t. the estimated parameter
     for the Mixture of Gaussian model.
     :param X: array of (n_samples, n_features)
-    :param mu: array of (n_features,), estimated mean
-    :param mu0: array of (n_features,), previously estimated mean
+    :param beta: array of (n_features,), estimated parameter
+    :param beta0: array of (n_features,), previously estimated parameter
     :param sigma: the fixed variance of the Gaussians
     :return G: array of (n_samples, n_features). Gradient at each sample.
     """
-    w = expit(X.dot(mu)[:, np.newaxis] / (sigma ** 2))
-    G = (2 * w - 1) * X - mu0
+    w = expit(X.dot(beta)[:, np.newaxis] / (sigma ** 2))
+    G = (2 * w - 1) * X - beta0
     return G
 
 
-def regression_mixture_grad(X, Y, mu, mu0, sigma):
+def regression_mixture_grad(X, Y, beta, beta0, sigma):
     """Compute the gradient on data set X w.r.t. the estimated parameter P
     for the Mixture of Regression model.
     :param X: array of (n_samples, n_features), data set
     :param Y: array of (n_samples,), objective of the regression
-    :param mu: array of (n_features,), estimated mean
-    :param mu0: array of (n_features,), previously estimated mean
+    :param beta: array of (n_features,), estimated parameter
+    :param beta0: array of (n_features,), previously estimated parameter
     :param sigma: the fixed variance of the Gaussian noises
     :return G:
     """
-    w = expit((X.dot(mu) * Y) / (sigma ** 2))
-    G = (2 * w * Y)[:, np.newaxis] * X - X.dot(mu0)[:, np.newaxis] * X
+    w = expit((X.dot(beta) * Y) / (sigma ** 2))
+    G = (2 * w * Y)[:, np.newaxis] * X - X.dot(beta0)[:, np.newaxis] * X
     return G
 
 
+def regression_without_cov_grad(X, Y, beta, beta0, sigma):
+    Z = np.ones(shape=X.shape)
+    Z = np.nan_to_num(Z)
+
+    raise NotImplementedError("function not implemented")
 
