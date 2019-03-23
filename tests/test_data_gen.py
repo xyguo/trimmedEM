bc432ebae42729cb5f8dd760f871eb04a4fe779e
xyguo_mac <xyguo.ubd@gmail.com>
2019-03-22 23:43:58 -0400

    1. Added tests for data generating functions. 2. Added parameters to specify effective dimention and background noise for the data generating funcs. 3. Added func for generating data from the Regression-with-Missing- Components model. 4. The default behavior of `add_outliers` func now is to produce a mutated copy of the data set passed in.

--- tests/test_data_gen.py
+++ tests/test_data_gen.py
@@ -2,18 +2,118 @@ import unittest
 import numpy as np
 
 from utils.data_gen import (
-    gaussian_mixture, regression_mixture, add_outliers
+    gaussian_mixture, regression_mixture, regression_with_missing_cov,
+    add_outliers
 )
 
 
 class TestUtils(unittest.TestCase):
 
     def test_gaussian_mixture(self):
-        pass
+        X1 = gaussian_mixture(n_samples=10,
+                              n_features=2,
+                              mean=np.array([1, 2]),
+                              sigma=1,
+                              bgn_sigma=0.01,
+                              subspace=None)
+        assert X1.shape == (10, 2)
+        assert np.all(X1 != 0)
+
+        # check if the `subspace` parameter takes effect
+        X2 = gaussian_mixture(n_samples=10,
+                              n_features=2,
+                              mean=np.array([1, 0]),
+                              sigma=1,
+                              bgn_sigma=0,
+                              subspace=np.array([0]))
+        assert X2.shape == (10, 2)
+        assert np.all(X2[:, 1] == 0)
+        assert np.all(X2[:, 0] != 0)
 
     def test_regression_mixture(self):
-        pass
+        X1, Y1 = regression_mixture(n_samples=10,
+                                    n_features=2,
+                                    mean=np.array([1, 2]),
+                                    sigma=1,
+                                    bgn_sigma=0.01,
+                                    subspace=None)
+        assert X1.shape == (10, 2)
+        assert Y1.shape == (10,)
+        assert np.all(X1 != 0)
+
+        # check if the `subspace` parameter takes effect
+        X2, Y2 = regression_mixture(n_samples=10,
+                                    n_features=2,
+                                    mean=np.array([1, 0]),
+                                    sigma=1,
+                                    bgn_sigma=0,
+                                    subspace=np.array([0]))
+        assert X2.shape == (10, 2)
+        assert Y2.shape == (10,)
+        assert np.all(X2[:, 1] == 0)
+        assert np.all(X2[:, 0] != 0)
+
+    def test_regression_with_missing_cov(self):
+        X1, Y1 = regression_with_missing_cov(n_samples=10,
+                                             n_features=2,
+                                             mean=np.array([1, 2]),
+                                             sigma=1,
+                                             missing_prob=0.5,
+                                             bgn_sigma=0.01,
+                                             subspace=None)
+        assert X1.shape == (10, 2)
+        assert Y1.shape == (10,)
+        assert np.all(X1 != 0)
+
+        # check if the `subspace` parameter takes effect
+        X2, Y2 = regression_mixture(n_samples=10,
+                                    n_features=2,
+                                    mean=np.array([1, 0]),
+                                    sigma=1,
+                                    bgn_sigma=0,
+                                    subspace=np.array([0]))
+        assert X2.shape == (10, 2)
+        assert Y2.shape == (10,)
+        assert np.all(np.logical_or(X2[:, 1] == 0, np.isnan(X2[:, 1])))
+        assert np.all(X2[:, 0] != 0)
 
     def test_add_outliers(self):
-        pass
+        # check if the X is changed when `inplace` is True
+        X = np.array([[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]])
+        X_orig = X.copy()
+
+        X3 = add_outliers(X, n_outliers=2, replace=True, inplace=True)
+        assert X3.shape == (3, 3)
+        assert X3 is X
+        assert np.any(X3 != X_orig, axis=1).sum() == 2  # check if exactly two rows are mutated
+
+        # check if the X is not changed when `inplace` is False
+        X = np.array([[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]])
+        X_orig = X.copy()
+        X4 = add_outliers(X, n_outliers=2, replace=True, inplace=False)
+        assert X4.shape == (3, 3)
+        assert X4 is not X
+        assert np.all(X_orig == X)
+        assert np.any(X4 != X_orig, axis=1).sum() == 2  # check if exactly two rows are mutated
+
+        # check X's shape is preserved when the `replace` is True
+        X = np.array([[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]])
+
+        X1 = add_outliers(X, n_outliers=2, replace=True, inplace=False)
+        assert X1.shape == (3, 3)
+        assert np.any(X1 != X, axis=1).sum() == 2  # check if exactly two rows are mutated
+
+        # check X's shape is changed when the `replace` is False
+        X = np.array([[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]])
+
+        X2 = add_outliers(X, n_outliers=2, replace=False, inplace=False)
+        assert X2.shape == (5, 3)
 
--- utils/data_gen.py
+++ utils/data_gen.py
@@ -8,71 +8,164 @@ import scipy as sp
 
 def gaussian_mixture(n_samples, n_features,
                      mean,
-                     sigma=None,
-                     n_outliers=0,
-                     outliers_dist_factor=50,
+                     sigma=1,
+                     subspace=None,
+                     bgn_sigma=0,
                      random_state=None):
     """
     generate data according to gaussian mixture model
     :param n_samples: number of samples
     :param n_features: dimension of data generated
-    :param n_outliers: number of outliers
     :param mean: array of shape=(n_features,)
     :param sigma: covariance of the Gaussian
-    :param outliers_dist_factor:
+    :param subspace: None or array of shape=(n_effective_idxs,).
+        The 'effective dimensions' of data.
+        When subspace is a subset of [n_features], this results in sparse data set.
+        When subspace is None, then it's equivalent to subspace = np.arange(n_features).
+    :param bgn_sigma: float,
+        The standard variance of the background noise (N(0, bg_noise_sigma));
+        This noise appears across ALL dimensions (regardless of the `subspace` parameter)
+    :param random_state:
     :return Y: array of shape=(n_samples, n_features)
     """
     np.random.seed(random_state)
+    if subspace is not None:
+        assert np.all(subspace < n_features) and np.all(subspace >= 0)
     Y = np.zeros((n_samples, n_features))
-    Z = np.random.choice([-1, +1], n_samples, replace=True, p=[1, 1])
-    V = np.random.multivariate_normal(np.zeros(n_features),
-                                      np.identity(n_features) * sigma,
-                                      n_samples)
+    Z = np.random.choice([-1, +1], n_samples, replace=True, p=[0.5, 0.5])
+    V = np.zeros((n_samples, n_features))
 
-    Y[:] = Z[:, np.newaxis].dot(mean[np.newaxis, :]) + V
-    #TODO: add desired number of outliers
+    if subspace is None:
+        V[:] = np.random.multivariate_normal(np.zeros(n_features),
+                                             np.identity(n_features) * sigma,
+                                             n_samples)
+        Y[:] = Z[:, np.newaxis].dot(mean[np.newaxis, :]) + V
+    else:
+        n_effective_idxs = subspace.shape[0]
+        V[:, subspace] = np.random.multivariate_normal(np.zeros(n_effective_idxs),
+                                                       np.identity(n_effective_idxs) * sigma,
+                                                       n_samples)
+        Y[:] = Z[:, np.newaxis].dot(mean[np.newaxis, :]) + V
+        if bgn_sigma > 0:
+            background_noise = np.random.randn(n_samples, n_features) * bgn_sigma
+            Y[:] += background_noise
     return Y
 
 
 def regression_mixture(n_samples, n_features,
                        mean,
-                       sigma_noise=None,
-                       n_outliers=0,
-                       outliers_dist_factor=50,
+                       sigma=1,
+                       subspace=None,
+                       bgn_sigma=0,
                        random_state=None):
     """
     generate data according to mixture of regression model
+    Y = Z \cdot X^T \beta + V
     :param n_samples: number of samples
-    :param n_clusters: number of Gaussians
-    :param n_outliers: number of outliers
     :param n_features: dimension of data generated
-    :param mean: array of shape=(n_features, )
-    :param sigma_noise: array of shape=(n_features, n_features)
-        covariance matrix for background noise
-    :param outliers_dist_factor:
-    :return X: array of shape=(n_samples, n_features)
+    :param mean: array of shape=(n_features, ), the `beta` in the model
+    :param sigma: array of shape=(n_features, n_features)
+        covariance matrix for noise (V) on observation variables
+    :param subspace: None or array of shape=(n_effective_idxs,).
+        The 'effective dimensions' of data.
+        When subspace is a subset of [n_features], this results in sparse data set.
+        When subspace is None, then it's equivalent to subspace = np.arange(n_features).
+    :param bgn_sigma: float,
+        The standard variance of the background noise (N(0, bg_noise_sigma));
+        This noise appears across ALL dimensions (regardless of the `subspace` parameter)
+    :return (X, Y): X is array of shape=(n_samples, n_features), Y is array of shape=(n_samples,)
     """
     np.random.seed(random_state)
-    X = np.random.multivariate_normal(np.zeros(n_features),
-                                      np.identity(n_features),
-                                      n_samples)
+    if subspace is not None:
+        assert np.all(subspace < n_features) and np.all(subspace >= 0)
+    X = np.zeros((n_samples, n_features))
     Y = np.zeros((n_samples, ))
-    Z = np.random.choice([-1, +1], n_samples, replace=True, p=[1, 1])
-    V = np.random.normal(0, sigma_noise, n_samples)
+    Z = np.random.choice([-1, +1], n_samples, replace=True, p=[0.5, 0.5])
+    V = np.random.normal(0, sigma, n_samples)
+
+    if subspace is None:
+        X[:] = np.random.multivariate_normal(np.zeros(n_features),
+                                             np.identity(n_features),
+                                             n_samples)
+    else:
+        n_effective_idxs = subspace.shape[0]
+        X[:, subspace] = np.random.multivariate_normal(np.zeros(n_effective_idxs),
+                                                       np.identity(n_effective_idxs),
+                                                       n_samples)
+        if bgn_sigma > 0:
+            background_noise = np.random.randn(n_samples, n_features) * bgn_sigma
+            X[:] += background_noise
 
     Y[:] = Z * (X.dot(mean)) + V
 
-    #TODO: add desired number of outliers
     return X, Y
 
 
-def add_outliers(X, n_outliers, dist_factor=10, random_state=None, return_index=False):
+def regression_with_missing_cov(n_samples, n_features,
+                                mean,
+                                missing_prob=0.2,
+                                sigma=1,
+                                subspace=None,
+                                bgn_sigma=0,
+                                random_state=None):
+    """
+    generate data according to the regression-with-missing-covariates model
+    Y = X^T \beta + V
+    :param n_samples: number of samples
+    :param n_features: dimension of data generated
+    :param mean: array of shape=(n_features, ), the `beta` in the model
+    :param missing_prob: float between 0 and 1.
+        each dim of X will be unobservable w.p. `missing_prob` i.i.d.
+    :param sigma: array of shape=(n_features, n_features)
+        covariance matrix for the noise (V) on observation variables
+    :param subspace: None or array of shape=(n_effective_idxs,).
+        The 'effective dimensions' of data.
+        When subspace is a subset of [n_features], this results in sparse data set.
+        When subspace is None, then it's equivalent to subspace = np.arange(n_features).
+    :param bgn_sigma: float,
+        The standard variance of the background noise (N(0, bg_noise_sigma));
+        This noise appears across ALL dimensions (regardless of the `subspace` parameter)
+    :return (X, Y): X is array of shape=(n_samples, n_features), Y is array of shape=(n_samples,)
+    """
+    np.random.seed(random_state)
+    if subspace is not None:
+        assert np.all(subspace < n_features) and np.all(subspace >= 0)
+    X = np.zeros((n_samples, n_features))
+    Y = np.zeros((n_samples, ))
+    Z = np.random.choice([1, np.nan], size=(n_samples, n_features),
+                         replace=True, p=[1-missing_prob, missing_prob])
+    V = np.random.normal(0, sigma, n_samples)
+
+    if subspace is None:
+        X[:] = np.random.multivariate_normal(np.zeros(n_features),
+                                             np.identity(n_features),
+                                             n_samples)
+    else:
+        n_effective_idxs = subspace.shape[0]
+        X[:, subspace] = np.random.multivariate_normal(np.zeros(n_effective_idxs),
+                                                       np.identity(n_effective_idxs),
+                                                       n_samples)
+        if bgn_sigma > 0:
+            background_noise = np.random.randn(n_samples, n_features) * bgn_sigma
+            X[:] += background_noise
+
+    Y[:] = X.dot(mean) + V
+
+    return Z * X, Y
+
+
+def add_outliers(X, n_outliers, dist_factor=10, replace=True,
+                 inplace=False, random_state=None, return_index=False):
     """
     add outliers to a given data set: add large random shifts to some of the data in X
     :param X: array of shape=(n_samples, n_features)
     :param n_outliers:
     :param dist_factor: int,
         affects how far away the outliers distributed
+    :param replace: bool, if true then perturb the original data set rather than adding
+        new points as outliers.
+    :param inplace: bool, default False.
+        Whether to modify X in-place or adding outliers to a copy of X.
     :param random_state:
     :param return_index: bool, whether to return
     :return X or (X, outliers):
@@ -89,6 +182,16 @@ def add_outliers(X, n_outliers, dist_factor=10, random_state=None, return_index=
     outliers_shift = np.random.multivariate_normal(np.zeros(n_features),
                                                    np.identity(n_features) * dist_factor * shift,
                                                    n_outliers)
-    outliers = X[outlier_idxs] + outliers_shift
-    X = np.vstack((X, outliers))
-    return (X, outlier_idxs) if return_index else X
+    if inplace:
+        # avoid modifying the input directly: remember, X is passed-in by reference
+        X_copy = X
+    else:
+        # X_copy refers to the same object as X
+        X_copy = X.copy()
+
+    if replace:
+        X_copy[outlier_idxs] = X_copy[outlier_idxs] + outliers_shift
+    else:
+        outliers = X_copy[outlier_idxs] + outliers_shift
+        X_copy = np.vstack((X, outliers))
+    return (X_copy, outlier_idxs) if return_index else X_copy
